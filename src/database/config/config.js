fc5aa9c93caae38289a6c6b4fe6d922e9526815b
Fidel Rojas <fidelrojas35@gmail.com>
2024-07-22 02:06:44 -0400

    remove docker

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'password',
+    username: 'dev',
+    password: 'strongone',
     database: 'elite-db',
     host: '127.0.0.1',
     dialect: 'postgres',
